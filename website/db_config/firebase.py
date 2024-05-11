69a0399a289613dd23ca23a7d655df45f62fd214
tranhuutuan261103 <tranhuutuan26112003@gmail.com>
2024-05-11 16:20:35 +0700

    Upload thumbnail when creating a post

--- website/db_config/firebase.py
+++ website/db_config/firebase.py
@@ -1,7 +1,9 @@
 from firebase_admin import credentials, firestore, initialize_app, storage
 
 cred = credentials.Certificate('./website/db_config/serviceAccountKey.json')
-default_app = initialize_app(cred)
+default_app = initialize_app(cred, {
+    'storageBucket': 'crypto-community-cb11a.appspot.com'
+})
 db = firestore.client()
 ArrayUnion = firestore.ArrayUnion
-bucket = storage.bucket(default_app.project_id)
+bucket = storage.bucket()
